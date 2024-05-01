ce5019ed952f19b7a1eba29053ee595d083e9b2a
Aqib Akram <aqibakram206@gmail.com>
2024-05-01 15:34:53 +0500

    first-commit

--- components/about.tsx
+++ components/about.tsx
@@ -19,9 +19,11 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        I am a Computer Scientist who just graduated from the premier IT
+        Institute{" "}
+        <span className="font-medium">in Computer Science Majors</span>, I
+        decided to pursue my passion for programming. I enrolled in a coding
+        bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
